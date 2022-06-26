7f1bb314465ac8d9e667443965a3076f8294da1b
ntabucejo <ntabucejo@gmail.com>
2022-06-26 20:58:18 +0800

    Initial nextauth setup

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  // adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+});
