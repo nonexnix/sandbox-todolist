90d43eca09efbf43efa42c731500e0fe136b9916
ntabucejo <ntabucejo@gmail.com>
2022-06-28 17:47:27 +0800

    Fix typo

--- providers/auth-provider.tsx
+++ providers/auth-provider.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const AuthProvider: React.FC<Props> = ({ children }) => {
   // If the session is not available and the user is not logged in, then redirect
-  const { session } = useAuth();q
+  const { session } = useAuth();
 
   // If the session is not available and the user is logged in, then show loader and render
   // Or  when the user tries to access restricted url and not logged in, then show loader and redirect
