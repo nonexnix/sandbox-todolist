2912cb96f3b7b1c5c96ac3e6204cf23664d05fa7
ntabucejo <ntabucejo@gmail.com>
2022-06-28 15:17:10 +0800

    Create todo api's

---
+++ pages/api/users/[userId]/todos/[todoId]/index.ts
@@ -0,0 +1,43 @@
+import { Prisma } from "@prisma/client";
+import { NextApiHandler } from "next";
+import { getSession } from "next-auth/react";
+import prismaClient from "../../../../../../adapters/prisma/prisma-client";
+
+const handler: NextApiHandler = async (request, response) => {
+  const session = await getSession({ req: request });
+
+  if (!session) {
+    response.send({ error: "Not logged in" });
+  }
+
+  try {
+    const todoId = String(request.query.todoId);
+    switch (request.method) {
+      case "PUT": {
+        const body: Prisma.TodoUpdateInput = JSON.parse(request.body);
+        const todo = await prismaClient.todo.update({
+          where: { id: todoId },
+          data: {
+            text: body.text,
+            isCompleted: body.isCompleted,
+            order: body.order,
+          },
+        });
+        return response.send(todo);
+      }
+      case "DELETE": {
+        const todo = await prismaClient.todo.delete({
+          where: { id: todoId },
+        });
+        return response.send(todo);
+      }
+      default: {
+        return response.send({ error: "Method not allowed" });
+      }
+    }
+  } catch (error) {
+    response.send({ error });
+  }
+};
+
+export default handler;
---
+++ pages/api/users/[userId]/todos/index.ts
@@ -0,0 +1,43 @@
+import { Prisma } from "@prisma/client";
+import { NextApiHandler } from "next";
+import { getSession } from "next-auth/react";
+import prismaClient from "../../../../../adapters/prisma/prisma-client";
+
+const handler: NextApiHandler = async (request, response) => {
+  const session = await getSession({ req: request });
+
+  if (!session) {
+    response.send({ error: "Not logged in" });
+  }
+
+  try {
+    const userId = String(request.query.userId);
+    switch (request.method) {
+      case "GET": {
+        const todos = await prismaClient.todo.findMany({
+          where: { userId },
+        });
+        return response.send(todos);
+      }
+      case "POST": {
+        const body: Prisma.TodoCreateInput = JSON.parse(request.body);
+        const todo = await prismaClient.todo.create({
+          data: {
+            text: body.text,
+            isCompleted: body.isCompleted,
+            order: body.order,
+            userId,
+          },
+        });
+        return response.send(todo);
+      }
+      default: {
+        return response.send({ error: "Method not allowed" });
+      }
+    }
+  } catch (error) {
+    response.send({ error });
+  }
+};
+
+export default handler;
