be8bf4ea8f80378ea5c589d38d77080fcab23571
ntabucejo <ntabucejo@gmail.com>
2022-06-28 17:47:18 +0800

    Initial page setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,52 @@
+import { User } from "@prisma/client";
 import type { GetServerSideProps, NextPage } from "next";
-import { useSession, signIn, signOut, getSession } from "next-auth/react";
+import { getSession } from "next-auth/react";
+import Link from "next/link";
+import prismaClient from "../adapters/prisma/prisma-client";
+import Path from "../constants/path";
+import useAuth from "../hooks/useAuth";
 
-const Home: NextPage = () => {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user!.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+interface Props {
+  user: User;
+}
+
+const LandingPage: NextPage<Props> = ({ user }) => {
+  const { session, signOut, signIn } = useAuth();
+
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>
+      1
+      <section>
+        {session ? (
+          <div>
+            <button onClick={() => signOut()}>Sign out</button>
+            <Link href={`${Path.USER}/${user.id}`}>Home</Link>
+          </div>
+        ) : (
+          <button onClick={() => signIn("github")}>Sign in</button>
+        )}
+      </section>
+    </div>
   );
 };
 
-export default Home;
+export default LandingPage;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
 
-  if (!session) {
-    return {
-      props: {
-        session,
-      },
-    };
-  }
+  if (!session) return { props: { session } };
+
+  const userEmail = String(session.user!.email);
+
+  const user = await prismaClient.user.findUnique({
+    where: { email: userEmail },
+  });
 
   return {
     props: {
       session,
+      user,
     },
   };
 };
---
+++ pages/user/[userId]/index.tsx
@@ -0,0 +1,53 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import prismaClient from "../../../adapters/prisma/prisma-client";
+import serializer from "../../../utilities/serializer";
+import SWRProvider from "../../../providers/swr-provider";
+import AuthProvider from "../../../providers/auth-provider";
+
+interface Props {
+  fallback: any;
+}
+
+const UserPage: NextPage<Props> = ({ fallback }) => {
+  return (
+    <AuthProvider>
+      <SWRProvider fallback={fallback}>
+        <section>Hello World!</section>
+      </SWRProvider>
+    </AuthProvider>
+  );
+};
+
+export default UserPage;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const users = await prismaClient.user.findMany();
+
+  const paths = users.map((user) => ({ params: { userId: user.id } }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const userId = String(params!.userId);
+
+  const user = await prismaClient.user.findUnique({
+    where: { id: userId },
+  });
+
+  const todos = await prismaClient.todo.findMany({
+    where: { userId },
+  });
+
+  return {
+    props: {
+      fallback: {
+        [`/api/users/${userId}`]: serializer(user),
+        [`/api/users/${userId}/todos`]: serializer(todos),
+      },
+    },
+  };
+};
