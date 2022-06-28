128af84fddebf2be8862e423789b624d14455281
ntabucejo <ntabucejo@gmail.com>
2022-06-28 15:19:37 +0800

    Create useTodos hook

---
+++ hooks/useTodos.ts
@@ -0,0 +1,62 @@
+import { Todo } from "@prisma/client";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+const useTodos = () => {
+  const router = useRouter();
+  const userId = String(router.query.userId);
+  const key = `/api/users/${userId}/todos`;
+  const { data, mutate } = useSWR(key);
+  const todos = data as Todo[];
+
+  return {
+    todos,
+    mutate: {
+      create: {
+        todo: async (payload: Todo) => {
+          mutate([...todos, payload]);
+          await fetch(key, {
+            method: "POST",
+            headers: {
+              contentType: "application/json",
+            },
+            body: JSON.stringify(payload),
+          });
+          mutate();
+        },
+        update: {
+          todo: async (payload: Todo) => {
+            mutate(
+              todos.map((todo) => {
+                if (todo.id !== payload.id) return todo;
+                return payload;
+              })
+            );
+            await fetch(`${key}/${payload.id}`, {
+              method: "PUT",
+              headers: {
+                contentType: "application/json",
+              },
+              body: JSON.stringify(payload),
+            });
+            mutate();
+          },
+        },
+        delete: {
+          todo: async (payload: { todoId: string }) => {
+            mutate(todos.filter((todo) => todo.id === payload.todoId));
+            await fetch(`${key}/${payload.todoId}`, {
+              method: "DELETE",
+              headers: {
+                contentType: "application/json",
+              },
+            });
+            mutate();
+          },
+        },
+      },
+    },
+  };
+};
+
+export default useTodos;
