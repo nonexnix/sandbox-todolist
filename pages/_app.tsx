f3a884644c08acc19276f5527993d53e2126172d
ntabucejo <ntabucejo@gmail.com>
2022-06-28 20:06:16 +0800

    Add head for seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>Sandbox Todolist</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
